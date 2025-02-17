fb82b44a5e0848738ea0bee9996a5258f98ed5a0
Md Alamin <alamin3636md@example.com>
2025-02-17 12:22:37 +0600

    product details update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 app.use(cors());
@@ -32,6 +32,16 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    
+    app.get('/movie/:id',async(req,res)=>{
+      const id = req.params.id;
+      console.log(id)
+      const query = {_id: new ObjectId(id)}
+      console.log(query)
+      const result = await moviecollection.findOne(query)
+      res.send(result)
+      console.log(result)
+    })
     app.post('/movie',async(req,res)=>{
       const newmovie = req.body;
       console.log(newmovie)
