b02aba14b7fbd2b13410e693422d54886806efb2
Md Alamin <alamin3636md@example.com>
2025-02-18 09:01:24 +0600

    dlt comment

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.MOVIE_USER}:${process.env.MOVIE_PASS}@cluster0.s3d9m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -21,8 +20,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    //     await client.connect();
+    
     app.get("/", (req, res) => {
       res.send(" client site movie check out");
     });
@@ -55,7 +53,7 @@ async function run() {
       const result = await moviecollection.deleteOne(cursor)
       res.send(result)
     })
-// update
+
     app.get('/movie/:id',async(req,res)=>{
       const id= req.params.id;
       const query = {_id: new ObjectId(id)}
@@ -85,14 +83,12 @@ async function run() {
 
     
 
-    // Send a ping to confirm a successful connection
-    //     await client.db("admin").command({ ping: 1 });
+  
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
-    //     await client.close();
+ 
   }
 }
 run().catch(console.dir);
